e605f2db64ec3fbfc9ad3edd1362cf638fc5494e
Orkidea10 <132389993+Orkidea10@users.noreply.github.com>
2025-06-03 17:44:02 +0200

    polimorfizem

---
+++ Moduli9/Polimorfizem.py
@@ -0,0 +1,23 @@
+string_length = len("Helloo")
+list_length = len([12, 321, 53, 6, 46])
+tuple_length = len((13, 215, 65))
+
+print(string_length)
+print(list_length)
+print(tuple_length)
+
+sum_of_list = sum([1,2,3,4,5])
+sum_of_tuple = sum((1, 2, 43))
+
+print(sum_of_list)
+print(sum_of_tuple)
+
+list_max= max([1, 4, 2])
+string_min= min("apple")
+
+print(list_max)
+print(string_min)
+
+list_abs = abs(-6)
+
+print(list_abs)
---
+++ Moduli9/poli_function.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    return x+y
+
+def concatenate(x,y):
+    return str(x)+ str(y)
+
+def operate(operation,x,y):
+    return operation(x,y)
+
+result_add= operate(add,2, 4)
+result_concatenate= operate(concatenate, "hello", " orkidea")
+
+print(result_add)
+print(result_concatenate)
